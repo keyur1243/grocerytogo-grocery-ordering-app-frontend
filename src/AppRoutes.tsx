ba5d97bcf7157ed91de4b4c6d37eff8eb787bf19
keyur1243 <129684868+keyur1243@users.noreply.github.com>
2024-04-18 09:28:32 -0400

    Update AppRoutes.tsx

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,27 +4,42 @@ import HomePage from "./pages/HomePage"
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import AdminProtectedRoute from "./auth/AdminProtectedRoute";
 import ManageGroceryStorePage from "./pages/ManageGroceryStorePage";
 import SearchPage from "./pages/SearchPage";
 import DetailPage from "./pages/DetailPage";
 import OrderStatusPage from "./pages/OrderStatusPage";
+import { UserProvider } from "./contexts/UserContext";
+
+
 const AppRoutes= ()=>{
     return(
+
+        <UserProvider>
         <Routes>
             <Route path="/" element={<Layout showHero><HomePage/></Layout>}/>
             <Route path="/auth-callback" element={<AuthCallbackPage />} />
             <Route path="/search/:city" element={<Layout showHero={false}><SearchPage/></Layout>} />
             <Route path="/detail/:groceryStoreId"element={ <Layout showHero={false}><DetailPage /></Layout>}/>
+            
+            {/* protected routes */}
             <Route element={<ProtectedRoute />}>
                 <Route path="/order-status" element={<Layout><OrderStatusPage/></Layout>} />
                 <Route path="/user-profile" element={<Layout><UserProfilePage/></Layout>} />
+            </Route>        
+
+            {/* admin protected routes */}
+            <Route element={<AdminProtectedRoute />}>
                 <Route path="/manage-groceryStore" element={<Layout><ManageGroceryStorePage/></Layout>} />
             </Route>        
+            
+            
             <Route path="/about" element={<span>About</span>}/>
             <Route path="/register" element={<span>Register</span>}/>
             <Route path="/shop" element={<span>Shop</span>}/>
             <Route path="*" element={<Navigate to="/"/>}/>
         </Routes>
+        </UserProvider>
     );
 };
 
