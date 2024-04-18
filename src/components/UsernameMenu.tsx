0c382f5abc3e442be679c96637f9a70dfdb6b799
keyur1243 <129684868+keyur1243@users.noreply.github.com>
2024-04-18 09:42:55 -0400

    Update UsernameMenu.tsx

--- src/components/UsernameMenu.tsx
+++ src/components/UsernameMenu.tsx
@@ -1,12 +1,20 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from  "./ui/dropdown-menu";
-import {  CircleUserRound } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { CircleUserRound } from "lucide-react";
 import { Link } from "react-router-dom";
 import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { useUser } from "@/contexts/UserContext";
+
 const UsernameMenu = () => {
+
     const { user, logout } = useAuth0();
+
+    // current user
+    const { currentUser } = useUser();
+
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger className="flex item-center px-3 font-bold hover:text-orange-500 gap-2">
@@ -14,16 +22,35 @@ const UsernameMenu = () => {
                 {user?.email}
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-            <DropdownMenuItem>
-                <Link to="/manage-groceryStore" className="font-bold hover:text-orange-500">Manage Grocery-Store</Link>
-                </DropdownMenuItem>
+
+                { 
+
+                /* show manage store link only when logged in user is admin */
+                
+                (currentUser && currentUser.userType == 'admin')
+
+                ? (                 
+                    <DropdownMenuItem>
+                        <Link to="/manage-groceryStore" className="font-bold hover:text-orange-500 px-3 p-1 block">Manage Grocery Store</Link>
+                    </DropdownMenuItem>
+                )
+
+                : null
+                                
+                }
+                
+
                 <DropdownMenuItem>
-                <Link to="/user-profile" className="font-bold hover:text-orange-500">User Profile</Link>
+                    <Link to="/user-profile" className="font-bold hover:text-orange-500 px-3 p-1 block">User Profile</Link>
                 </DropdownMenuItem>
                 <Separator />
+                
                 <DropdownMenuItem>
-                    <Button className="flex flex-1 font-bold bg-orange-500" onClick={() => logout()}>Log Out</Button>
+                    <div className="px-2">
+                       <Button className="flex flex-1 font-bold bg-orange-500 mt-3" onClick={() => logout()}>Log Out</Button>
+                    </div>
                 </DropdownMenuItem>
+                
             </DropdownMenuContent>
         </DropdownMenu>
     )
