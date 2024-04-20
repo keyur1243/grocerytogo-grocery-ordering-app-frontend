26541305e0f5bf00e5b5baa284ecaa704b1f13f5
keyur1243 <129684868+keyur1243@users.noreply.github.com>
2024-04-20 09:18:26 -0400

    Update Carousel.tsx

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useGetRandomGroceryStores } from '@/api/GroceryStoreApi';
 import SearchResultCard from './SearchResultCard';
 
